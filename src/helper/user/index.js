a9203e241d839dfaad4220b4f5c6229c38ed10b7
João Pedro Arruda <jaaopbr@gmail.com>
2021-04-08 18:32:22 -0300

    ✨ Feat: Added user endpoints

---
+++ src/helper/user/index.js
@@ -0,0 +1,4 @@
+export const postLogin = () => `/login`;
+export const postRegister = () => `/register`;
+export const getUser = (id) => `/users/${id}`;
+export const patchUser = (id) => `/users/${id}`;
