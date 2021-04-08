f32adb78650bd640cb7ad36f665e5a9479cc3d3d
vinicius <vinicius.campello@hotmail.com>
2021-04-08 14:48:00 -0300

    feat: update branch.

--- src/routes/routes.jsx
+++ src/routes/routes.jsx
@@ -1,40 +1,42 @@
-import { Switch, Route } from "react-router-dom";
+import {Switch, Route} from "react-router-dom";
+
+// Junia Update
 
 const Routes = () => {
-  return (
-    <Switch>
-      <Route exact path="/">
-        home
-      </Route>
+	return (
+		<Switch>
+			<Route exact path="/">
+				home
+			</Route>
 
-      <Route exact path="/login">
-        login
-      </Route>
+			<Route exact path="/login">
+				login
+			</Route>
 
-      <Route exact path="/register">
-        register
-      </Route>
+			<Route exact path="/register">
+				register
+			</Route>
 
-      <Route exact path="/dashboard">
-        dashboard
-      </Route>
+			<Route exact path="/dashboard">
+				dashboard
+			</Route>
 
-      <Route exact path="/store/:id">
-        store
-      </Route>
+			<Route exact path="/store/:id">
+				store
+			</Route>
 
-      <Route exact path="/store/profile/:id">
-        store - profile
-      </Route>
+			<Route exact path="/store/profile/:id">
+				store - profile
+			</Route>
 
-      <Route exact path="/cart">
-        cart
-      </Route>
+			<Route exact path="/cart">
+				cart
+			</Route>
 
-      <Route exact path="/profile">
-        profile
-      </Route>
-    </Switch>
-  );
+			<Route exact path="/profile">
+				profile
+			</Route>
+		</Switch>
+	);
 };
 export default Routes;
