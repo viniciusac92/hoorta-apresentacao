1397525a86fc67fe2976122d74077c1519591e57
João Pedro Arruda <jaaopbr@gmail.com>
2021-04-09 11:21:29 -0300

    🥅 Feat: Added API error handling

--- src/components/molecules/FormLogin/index.jsx
+++ src/components/molecules/FormLogin/index.jsx
@@ -1,3 +1,6 @@
+// React
+import { useState } from "react";
+
 // API
 import API from "../../../services/api";
 
@@ -14,13 +17,8 @@ import { useHistory } from "react-router-dom";
 // ContextAPI
 import { useData } from "../../../providers/UserContext";
 
-// Styles
-
-// Components
-
-/* ------------------------------ */
-
 const FormLogin = () => {
+  const [error, setError] = useState(null);
   //   const history = useHistory();
   const { setUserData } = useData();
   const {
@@ -34,24 +32,24 @@ const FormLogin = () => {
 
   const handleForm = async (data) => {
     try {
-      // Faz login e salva token
       const response = await API.post(postLogin(), data);
       localStorage.setItem("token", JSON.stringify(response.data.accessToken));
-      const { sub } = jwt_decode(response.data.accessToken);
-
-      //@TODO Armazenar o token no context
-      // Busca dados do usuário e salva os dados no context
+      const { sub } = jwt_decode(response.data.accessToken); // Id
       const userInfo = await API.get(getUser(sub), {
         headers: {
           Authorization: `Bearer ${JSON.parse(localStorage.getItem("token"))}`,
         },
       });
       setUserData({ ...userInfo.data });
-
       reset();
       //   history.push();
-    } catch (error) {
-      console.log(error);
+    } catch (e) {
+      console.log(e.response.data);
+      if (e.response.data === "Cannot find user") {
+        setError("Usuário não encontrado");
+      } else if (e.response.data === "Incorrect password") {
+        setError("Senha incorreta");
+      }
     }
   };
 
@@ -61,6 +59,7 @@ const FormLogin = () => {
       <p>{errors.email?.message}</p>
       <input type="password" placeholder="Senha" {...register("password")} />
       <p>{errors.password?.message}</p>
+      <p>{error && error}</p>
       <button type="submit">Enviar</button>
     </form>
   );
