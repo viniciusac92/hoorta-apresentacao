78d98fbd2e6fd467af522db8fecb985cc7da67ad
João Pedro Arruda <jaaopbr@gmail.com>
2021-04-09 16:08:08 -0300

    🔥 Chore: Removed code

--- src/App.js
+++ src/App.js
@@ -1,20 +1,8 @@
-import Logo from "./components/atoms/Logo";
-import Input from "./components/atoms/Input";
-import Button from "./components/atoms/Button";
-import TextHead from "./components/atoms/TextHeader";
+import Routes from "./routes/routes";
 const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <Logo />
-        <TextHead styled={"black"} size={"small"} fontWeight={"bold"}>
-          Login
-        </TextHead>
-        <Input size={"small"}></Input>
-        <Button styled={"primary"} size={"small"}>
-          Entrar
-        </Button>
-      </header>
+    <div>
+      <Routes />
     </div>
   );
 };
