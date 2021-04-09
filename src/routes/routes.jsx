741ecabf2077186ff627e74416dd673a4e2798cd
João Pedro Arruda <jaaopbr@gmail.com>
2021-04-09 16:06:43 -0300

    🚚 Feat: Added /tests route

--- src/routes/routes.jsx
+++ src/routes/routes.jsx
@@ -1,42 +1,45 @@
-import {Switch, Route} from "react-router-dom";
-
+import { Switch, Route } from "react-router-dom";
 // Junia Update
+// Pages
+import Tests from "../pages/Tests";
 
 const Routes = () => {
-	return (
-		<Switch>
-			<Route exact path="/">
-				home
-			</Route>
+  return (
+    <Switch>
+      <Route exact path="/">
+        Home
+      </Route>
 
-			<Route exact path="/login">
-				login
-			</Route>
+      <Route exact path="/login">
+        login
+      </Route>
 
-			<Route exact path="/register">
-				register
-			</Route>
+      <Route exact path="/register">
+        register
+      </Route>
 
-			<Route exact path="/dashboard">
-				dashboard
-			</Route>
+      <Route exact path="/dashboard">
+        dashboard
+      </Route>
 
-			<Route exact path="/store/:id">
-				store
-			</Route>
+      <Route exact path="/store/:id">
+        store
+      </Route>
 
-			<Route exact path="/store/profile/:id">
-				store - profile
-			</Route>
+      <Route exact path="/store/profile/:id">
+        store - profile
+      </Route>
 
-			<Route exact path="/cart">
-				cart
-			</Route>
+      <Route exact path="/cart">
+        cart
+      </Route>
 
-			<Route exact path="/profile">
-				profile
-			</Route>
-		</Switch>
-	);
+      <Route exact path="/profile">
+        profile
+      </Route>
+
+      <Route exact path="/tests" component={Tests} />
+    </Switch>
+  );
 };
 export default Routes;
