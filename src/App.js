5def0eb90eac597c87d0637c88dbe4bfca12ec5b
Mikewillv94 <mikeumsm@gmail.com>
2021-04-09 13:21:02 -0300

    text header completo

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import Logo from "./components/atoms/Logo";
 import Input from "./components/atoms/Input";
 import Button from "./components/atoms/Button";
-
+import TextHead from "./components/atoms/TextHeader";
 const App = () => {
   return (
     <div className="App">
       <header className="App-header">
         <Logo />
+        <TextHead styled={"black"} size={"small"} fontWeight={"bold"}>
+          Login
+        </TextHead>
         <Input size={"small"}></Input>
         <Button styled={"primary"} size={"small"}>
           Entrar
