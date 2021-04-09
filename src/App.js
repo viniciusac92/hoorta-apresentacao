694900b0805efed21c74ae4c6bf012ecec62bc4d
Junia Nunes <jununes.bermejo@gmail.com>
2021-04-09 14:17:46 -0300

    ✨ Feat: Added atom Picture and images folders

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import Logo from "./components/atoms/Logo";
 
 const App = () => {
-	return (
-		<div>
-			Hello React!
-			<Logo />
-		</div>
-	);
+  return (
+    <div>
+      Hello React!
+      <Logo />
+    </div>
+  );
 };
 
 export default App;
