c6e8f198bd18f30ee1224268c3953372f727c0ea
Lucas Passeli <lucaspasseli.work@gmail.com>
2021-04-09 19:58:29 -0300

    ✨ feat: Added molecule Header NavBar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
+import Tests from "./pages/Tests";
 import Routes from "./routes/routes";
 const App = () => {
-	return (
-		<div>
-			<Routes />
-		</div>
-	);
+  return (
+    <div>
+      <Routes />
+      <Tests />
+    </div>
+  );
 };
 
 export default App;
--- src/pages/Tests/index.jsx
+++ src/pages/Tests/index.jsx
@@ -2,11 +2,13 @@ import Button from "../../components/atoms/Button";
 import Input from "../../components/atoms/Input";
 
 import FormLogin from "../../components/molecules/FormLogin";
+import HeaderNavBar from "../../components/molecules/HeaderNavBar";
 
 const Tests = () => {
   return (
     <div>
       <FormLogin />
+      <HeaderNavBar />
     </div>
   );
 };
