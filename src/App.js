604a065306e5ac5cba9756a887e66fe8842c7862
vinicius <vinicius.campello@hotmail.com>
2021-04-09 16:12:42 -0300

    feat: Icons added.

--- src/App.js
+++ src/App.js
@@ -1,19 +1,9 @@
-import Logo from "./components/atoms/Logo";
-import Input from "./components/atoms/Input";
-import Button from "./components/atoms/Button";
-import TextHead from "./components/atoms/TextHeader";
+import Icon from "./components/atoms/Icon";
 const App = () => {
 	return (
 		<div className="App">
 			<header className="App-header">
-				<Logo />
-				<TextHead styled={"black"} size={"small"} fontWeight={"bold"}>
-					Login
-				</TextHead>
-				<Input size={"small"}></Input>
-				<Button styled={"primary"} size={"small"}>
-					Entrar
-				</Button>
+				<Icon />
 			</header>
 		</div>
 	);
