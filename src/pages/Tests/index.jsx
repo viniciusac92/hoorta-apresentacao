bdc2cf64d6f096caec66f58703cb22f86d552707
João Pedro Arruda <jaaopbr@gmail.com>
2021-04-10 14:11:36 -0300

    💄Style: Updated styles

--- src/pages/Tests/index.jsx
+++ src/pages/Tests/index.jsx
@@ -2,11 +2,13 @@ import Button from "../../components/atoms/Button";
 import Input from "../../components/atoms/Input";
 
 import FormLogin from "../../components/molecules/FormLogin";
+import ModalUser from "../../components/organisms/ModalUser";
 
 const Tests = () => {
   return (
     <div>
       <FormLogin />
+      <ModalUser />
     </div>
   );
 };
