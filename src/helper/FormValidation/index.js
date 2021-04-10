b407c9b454ebbc9c38aa3dda4354e99e2cce0bb9
João Pedro Arruda <jaaopbr@gmail.com>
2021-04-10 00:16:05 -0300

    ✨ Feat: Added regex and error messages

--- src/helper/FormValidation/index.js
+++ src/helper/FormValidation/index.js
@@ -1,5 +1,8 @@
 import * as yup from "yup";
 
+// REGEX
+const PHONE_REGEX = /^\([1-9]{2}\) [9]{1}[6-9]{1}[0-9]{3}\-[0-9]{4}$/;
+
 // Error Messages
 const required = "Campo obrigatório";
 
@@ -10,4 +13,12 @@ export const loginSchema = yup.object().shape({
     .string()
     .min(6, "A senha deve conter no mínimo 6 caracteres")
     .required(required),
+});
+
+export const updateUserSchema = yup.object().shape({
+  name: yup.string().max(20, "Máximo 10 caracteres"),
+  phone: yup.string().matches(PHONE_REGEX, {
+    message: "Número inválido, formato requerido: (11) 9111-1111",
+    excludeEmptyString: true,
+  }),
 });
