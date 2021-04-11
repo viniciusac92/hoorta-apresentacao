2f6866ecb7d7fdff615e43ebc81d836ca6e197a1
João Pedro Arruda <jaaopbr@gmail.com>
2021-04-10 22:07:22 -0300

    ⚰️Fix: Removed useless param

--- src/helper/stores/index.js
+++ src/helper/stores/index.js
@@ -1,4 +1,4 @@
-export const getStores = (id) => `/stores`;
+export const getStores = () => `/stores`;
 export const getOneStore = (id) => `/stores/${id}`;
 export const postStore = () => `/stores`;
 export const patchStore = (id) => `/stores/${id}`;
