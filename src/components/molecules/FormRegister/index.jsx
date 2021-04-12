b2cac72ffd0fe3fef773e2b0a47df043435f5ef4
vinicius <vinicius.campello@hotmail.com>
2021-04-12 12:07:22 -0300

    ✅ feat: add FormRegister into Register page

--- src/components/molecules/FormRegister/index.jsx
+++ src/components/molecules/FormRegister/index.jsx
@@ -1,93 +1,94 @@
 // React
-import { createRef, useState } from "react";
+import {createRef, useState} from "react";
 // API
 import API from "../../../services/api";
 // Helpers
-import { postRegister } from "../../../helper/user";
-import { registerSchema } from "../../../helper/FormValidation";
+import {postRegister} from "../../../helper/user";
+import {registerSchema} from "../../../helper/FormValidation";
 // Dependencies
-import { useForm } from "react-hook-form";
-import { yupResolver } from "@hookform/resolvers/yup";
-import { useHistory } from "react-router-dom";
+import {useForm} from "react-hook-form";
+import {yupResolver} from "@hookform/resolvers/yup";
+import {useHistory} from "react-router-dom";
 // ContextAPI
-import { useData } from "../../../providers/UserContext";
+import {useData} from "../../../providers/UserContext";
 // Components
 import Input from "../../atoms/Input";
 import Button from "../../atoms/Button";
+import FormStyled from "./styles";
 
 const FormRegister = () => {
-  const ref = createRef();
-  const [error, setError] = useState(null);
-  const history = useHistory();
-  const {
-    register,
-    handleSubmit,
-    formState: { errors },
-    reset,
-  } = useForm({
-    resolver: yupResolver(registerSchema),
-  });
+	const ref = createRef();
+	const [error, setError] = useState(null);
+	const history = useHistory();
+	const {
+		register,
+		handleSubmit,
+		formState: {errors},
+		reset,
+	} = useForm({
+		resolver: yupResolver(registerSchema),
+	});
 
-  const handleForm = async (data) => {
-    try {
-      await API.post(postRegister(), {
-        ...data,
-        phone: "",
-        address: [],
-        cc: [],
-        productor: false,
-        shoppingCount: 0,
-      });
-      reset();
-      // history.push("/login");
-    } catch (e) {
-      console.log(e);
-      if (e.response.data === "Email already exists") {
-        setError("Email já cadastrado");
-      }
-    }
-  };
+	const handleForm = async (data) => {
+		try {
+			await API.post(postRegister(), {
+				...data,
+				phone: "",
+				address: [],
+				cc: [],
+				productor: false,
+				shoppingCount: 0,
+			});
+			reset();
+			// history.push("/login");
+		} catch (e) {
+			console.log(e);
+			if (e.response.data === "Email already exists") {
+				setError("Email já cadastrado");
+			}
+		}
+	};
 
-  return (
-    <form onSubmit={handleSubmit(handleForm)}>
-      <Input
-        ref={ref}
-        type="text"
-        placeholder="Email"
-        size="large"
-        {...register("email")}
-      />
-      <p>{errors.email?.message}</p>
-      <Input
-        ref={ref}
-        type="password"
-        placeholder="Senha"
-        size="large"
-        {...register("password")}
-      />
-      <p>{errors.password?.message}</p>
-      <Input
-        ref={ref}
-        type="text"
-        placeholder="Nome"
-        size="large"
-        {...register("name")}
-      />
-      <p>{errors.name?.message}</p>
-      <Input
-        ref={ref}
-        type="number"
-        placeholder="Idade"
-        size="large"
-        {...register("age")}
-      />
-      <p>{errors.age?.message}</p>
-      <p>{error && error}</p>
-      <Button type="submit" color="primary" size="large">
-        Cadastrar
-      </Button>
-    </form>
-  );
+	return (
+		<FormStyled onSubmit={handleSubmit(handleForm)}>
+			<Input
+				ref={ref}
+				type="text"
+				placeholder="Email"
+				size="large"
+				{...register("email")}
+			/>
+			<p>{errors.email?.message}</p>
+			<Input
+				ref={ref}
+				type="password"
+				placeholder="Senha"
+				size="large"
+				{...register("password")}
+			/>
+			<p>{errors.password?.message}</p>
+			<Input
+				ref={ref}
+				type="text"
+				placeholder="Nome"
+				size="large"
+				{...register("name")}
+			/>
+			<p>{errors.name?.message}</p>
+			<Input
+				ref={ref}
+				type="number"
+				placeholder="Idade"
+				size="large"
+				{...register("age")}
+			/>
+			<p>{errors.age?.message}</p>
+			<p>{error && error}</p>
+			<Button type="submit" color="primary" size="large">
+				Cadastrar
+			</Button>
+		</FormStyled>
+	);
 };
 
 export default FormRegister;
