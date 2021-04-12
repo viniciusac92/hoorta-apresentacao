9045de8adce527a1aecc4818b18ad398dc9aa8fd
vinicius <vinicius.campello@hotmail.com>
2021-04-12 20:47:46 -0300

    🎨 feat: update Develop

--- src/pages/Tests/index.jsx
+++ src/pages/Tests/index.jsx
@@ -10,40 +10,12 @@ import HomeInfo from "../../components/organisms/HomeInfo";
 import heartImage from "../../assets/images/heartImage.png";
 import vegetablePack from "../../assets/images/vegetablePack.png";
 import footerPicture from "../../assets/images/footerPicture.png";
+import ListProducts from "../../components/molecules/ListProducts";
 
 const Tests = () => {
 	return (
 		<div>
-			<Header />
-			<Meet />
-			<Figure type="square">
-				<Picture
-					image={heartImage}
-					width="437px"
-					height="412px"
-					top="30px"
-					left="-130px"
-					position="relative"
-				/>
-			</Figure>
-			<Figure type="rectangle">
-				<Picture
-					image={vegetablePack}
-					width="305px"
-					height="444px"
-					top="100px"
-					left="85px"
-					position="relative"
-				/>
-			</Figure>
-			<Picture
-				image={footerPicture}
-				width="444px"
-				height="215px"
-				position="absolute"
-				top="100%"
-			/>
-			<HomeInfo />
+			<ListProducts />
 		</div>
 	);
 };
