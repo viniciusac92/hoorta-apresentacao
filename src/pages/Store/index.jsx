656d3f1aa4f03d3bc7ce56c549cf307a989d2f13
João Pedro Arruda <jaaopbr@gmail.com>
2021-04-12 16:11:41 -0300

    ✨Feat: Added Store context and store page by id

---
+++ src/pages/Store/index.jsx
@@ -0,0 +1,31 @@
+import { useEffect } from "react";
+import { useParams } from "react-router";
+import { getOneStore } from "../../helper/stores";
+import { useStores } from "../../providers/StoresContext";
+import API from "../../services/api";
+
+const Store = () => {
+  const { id } = useParams();
+  const { storeData, setStoreData } = useStores();
+
+  const getStoreData = async (id) => {
+    try {
+      const response = await API.get(getOneStore(id), {
+        headers: {
+          Authorization: `Bearer ${JSON.parse(localStorage.getItem("token"))}`,
+        },
+      });
+      setStoreData(response.data);
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+  useEffect(() => {
+    getStoreData(id);
+  }, []);
+
+  return <div>{console.log(storeData)}</div>;
+};
+
+export default Store;
