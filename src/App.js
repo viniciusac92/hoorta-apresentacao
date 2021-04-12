b6929fde852b0aeaac4381e82e4ded46def7fec7
vinicius <vinicius.campello@hotmail.com>
2021-04-12 19:04:46 -0300

    🎨 feat: App update

--- src/App.js
+++ src/App.js
@@ -2,12 +2,11 @@ import FormLogin from "./components/molecules/FormLogin";
 import Tests from "./pages/Tests";
 import Routes from "./routes/routes";
 const App = () => {
-  return (
-    <div>
-      <Routes />
-      <FormLogin />
-    </div>
-  );
+	return (
+		<div>
+			<Routes />
+		</div>
+	);
 };
 
 export default App;
