3b3521c7304e8f3912973a2fe2cb7cffaebe7ac1
Mikewillv94 <mikeumsm@gmail.com>
2021-04-13 17:52:08 -0300

    atualziado-resposividade

--- src/components/molecules/ListStores/style.js
+++ src/components/molecules/ListStores/style.js
@@ -8,7 +8,7 @@ export const List = styled.div`
     margin-left: 30px;
   }
 
-  @media screen and (min-width: 800px) {
+  @media screen and (max-width: 800px) {
     > div > p {
       text-align: center;
       font-size: 24px;
