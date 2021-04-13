f9b0b811b90ebba516121ca75263deb874a13389
Lucas Passeli <lucaspasseli.work@gmail.com>
2021-04-12 21:43:26 -0300

    ✨ feat: Addead a molecule SideBar

---
+++ src/components/molecules/SideBar/style.js
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+import theme from "../../../styles/theme";
+
+const SideBarStyled = styled.div`
+  width: 20vh;
+  box-shadow: ${theme.shadow};
+`;
+
+SideBarStyled.Container = styled.div``;
+
+SideBarStyled.Wrap = styled.div``;
+
+export default SideBarStyled;
