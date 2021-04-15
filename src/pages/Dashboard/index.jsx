da49a2c5d4aee9840c1fd46f7a582ef4bf2c00a4
João Pedro Arruda <jaaopbr@gmail.com>
2021-04-15 14:41:35 -0300

    💄Style: Updated pages template

--- src/pages/Dashboard/index.jsx
+++ src/pages/Dashboard/index.jsx
@@ -1,19 +1,11 @@
-import DashboardHeader from "../../components/organisms/DashboardHeader";
 import StoreSection from "../../components/organisms/StoresSection";
-import Sidebar from "../../components/molecules/SideBar";
-import { GridContainer } from "./styles";
+import MenuDashboard from "../../components/template/MenuDashboard";
 
 const Dashboard = () => {
   return (
-    <GridContainer>
-      <Sidebar />
-      <div className="HeaderContainer">
-        <DashboardHeader />
-      </div>
-      <div className="SectionContainer">
-        <StoreSection />
-      </div>
-    </GridContainer>
+    <MenuDashboard>
+      <StoreSection />
+    </MenuDashboard>
   );
 };
 
