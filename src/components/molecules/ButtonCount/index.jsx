aece8ee5b6a4c5dc379576cb4f88fe63f51ffbd9
Lucas Passeli <lucaspasseli.work@gmail.com>
2021-04-16 17:48:31 -0300

    🎨 fix: ajusting cartInfo and cardCart props

--- src/components/molecules/ButtonCount/index.jsx
+++ src/components/molecules/ButtonCount/index.jsx
@@ -5,17 +5,12 @@ import plus from "../../../assets/images/icons/plus.svg";
 import minus from "../../../assets/images/icons/minus.svg";
 import Text from "../../atoms/Text";
 import ButtonCountContainer from "./styles";
-<<<<<<< HEAD
 import { useState, useEffect } from "react";
-=======
-import {useState} from "react";
 import TextProduct from "../../atoms/TextProduct";
->>>>>>> c52a38ae32cf2218f166215751b763494c31c1f7
 
 const ButtonCount = ({ children, amountOfProduct, setAmountOfProduct }) => {
   const [amount, setAmount] = useState(1);
 
-<<<<<<< HEAD
   useEffect(() => {
     amountOfProduct === 1 && setAmount(amountOfProduct);
   }, [amountOfProduct]);
@@ -43,27 +38,6 @@ const ButtonCount = ({ children, amountOfProduct, setAmountOfProduct }) => {
       </ButtonMore>
     </ButtonCountContainer>
   );
-=======
-	return (
-		<ButtonCountContainer>
-			<ButtonLess
-				size="large"
-				onClick={() => number > 0 && setNumber(number - 1)}>
-				<Icon
-					src={minus}
-					display={["block", "block"]}
-					height={["3vw", "6vw"]}
-				/>
-			</ButtonLess>
-			<TextProduct size="medium" weigth="semiBold">
-				{number}
-			</TextProduct>
-			<ButtonMore size="large" onClick={() => setNumber(number + 1)}>
-				<Icon src={plus} display={["block", "block"]} />
-			</ButtonMore>
-		</ButtonCountContainer>
-	);
->>>>>>> c52a38ae32cf2218f166215751b763494c31c1f7
 };
 
 export default ButtonCount;
--- src/pages/Cart/index.jsx
+++ src/pages/Cart/index.jsx
@@ -2,18 +2,32 @@ import MenuDashboard from "../../components/template/MenuDashboard";
 import CartInfo from "../../components/molecules/CartInfo";
 import CartContainer from "../../components/organisms/CardContainer";
 import CardCart from "../../components/molecules/CardCart";
+import { useState, useEffect } from "react";
 
 const Cart = () => {
-	return (
-		<div>
-			<MenuDashboard>
-				<CartContainer>
-					<CardCart amout="5" />
-					<CartInfo />
-				</CartContainer>
-			</MenuDashboard>
-		</div>
-	);
+  const [cartProducts, setCartProducts] = useState([]);
+
+  useEffect(() => {
+    setCartProducts(JSON.parse(localStorage.getItem("cart")));
+  }, []);
+
+  return (
+    <div>
+      <MenuDashboard>
+        <CartContainer>
+          {cartProducts.map(({ product }, key) => (
+            <CardCart
+              amout={product?.info.amout}
+              price={product?.info.price}
+              productName={product?.info.name}
+              productorName={product?.info.description}
+            />
+          ))}
+          <CartInfo deliveryPrice="5" purchases={cartProducts} />
+        </CartContainer>
+      </MenuDashboard>
+    </div>
+  );
 };
 
 export default Cart;
