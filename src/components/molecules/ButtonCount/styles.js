3dffa201b0167cfe0b93f196b30db2fc370913bc
vinicius <vinicius.campello@hotmail.com>
2021-04-16 10:38:57 -0300

    🎨 feat: Responsive improve

--- src/components/molecules/ButtonCount/styles.js
+++ src/components/molecules/ButtonCount/styles.js
@@ -1,10 +1,10 @@
 import styled from "styled-components";
 
 const ButtonCountContainer = styled.div`
-	display: flex;
+	/* display: flex;
 	flex-direction: row;
 	justify-content: space-between;
-	align-items: center;
+	align-items: center; */
 `;
 
 export default ButtonCountContainer;
