37ff40dc6b632593d1a76d462e517fff78c0b7d2
vinicius <vinicius.campello@hotmail.com>
2021-04-17 15:18:45 -0300

    🚚 🎨 feat: Add MenuEditDelete into Products Sec.

--- src/components/molecules/MenuEditDelte/index.jsx
+++ src/components/molecules/MenuEditDelte/index.jsx
@@ -4,56 +4,54 @@ import Menu from "@material-ui/core/Menu";
 import Icon from "../../atoms/Icon";
 import Link from "../../atoms/Link";
 import threeDots from "../../../assets/images/icons/threeDots.svg";
-import { GroupLink, Item } from "./styles";
-import ModalDeleteProduct from "../../organisms/ModalProductDelete";
+import {GroupLink, Item} from "./styles";
+import ModalDeleteProduct from "../../organisms/ModalDeleteProduct";
 
 const ITEM_HEIGHT = 48;
 
-export default function MenuEditDelete({ id }) {
-  const [anchorEl, setAnchorEl] = React.useState(null);
-  const open = Boolean(anchorEl);
+export default function MenuEditDelete({id}) {
+	const [anchorEl, setAnchorEl] = React.useState(null);
+	const open = Boolean(anchorEl);
 
-  const handleClick = (event) => {
-    setAnchorEl(event.currentTarget);
-  };
+	const handleClick = (event) => {
+		setAnchorEl(event.currentTarget);
+	};
 
-  const handleClose = () => {
-    setAnchorEl(null);
-  };
+	const handleClose = () => {
+		setAnchorEl(null);
+	};
 
-  return (
-    <div>
-      <IconButton
-        aria-label="more"
-        aria-controls="long-menu"
-        aria-haspopup="true"
-        onClick={handleClick}
-      >
-        <Icon src={threeDots} alt="" display={["block", "block"]} />
-      </IconButton>
-      <Menu
-        id="long-menu"
-        anchorEl={anchorEl}
-        keepMounted
-        open={open}
-        onClose={handleClose}
-        PaperProps={{
-          style: {
-            maxHeight: ITEM_HEIGHT * 4.5,
-            width: "10ch",
-            padding: "0px",
-          },
-        }}
-      >
-        <GroupLink>
-          <Item type="button" onClick={handleClose}>
-            Editar
-          </Item>
-          <Item type="button" onClick={handleClose}>
-            <ModalDeleteProduct id={id} />
-          </Item>
-        </GroupLink>
-      </Menu>
-    </div>
-  );
+	return (
+		<div>
+			<IconButton
+				aria-label="more"
+				aria-controls="long-menu"
+				aria-haspopup="true"
+				onClick={handleClick}>
+				<Icon src={threeDots} alt="" display={["block", "block"]} />
+			</IconButton>
+			<Menu
+				id="long-menu"
+				anchorEl={anchorEl}
+				keepMounted
+				open={open}
+				onClose={handleClose}
+				PaperProps={{
+					style: {
+						maxHeight: ITEM_HEIGHT * 4.5,
+						width: "10ch",
+						padding: "0px",
+					},
+				}}>
+				<GroupLink>
+					<Item type="button" onClick={handleClose}>
+						Editar
+					</Item>
+					<Item type="button" onClick={handleClose}>
+						<ModalDeleteProduct id={id} />
+					</Item>
+				</GroupLink>
+			</Menu>
+		</div>
+	);
 }
