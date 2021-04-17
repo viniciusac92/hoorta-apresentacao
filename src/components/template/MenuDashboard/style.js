1869a47ba4eb71b77a75488115a8220e461b8ef7
Mikewillv94 <mikeumsm@gmail.com>
2021-04-17 19:37:47 -0300

    dashboard-e-dashboardHeader-atualizado

--- src/components/template/MenuDashboard/style.js
+++ src/components/template/MenuDashboard/style.js
@@ -2,44 +2,43 @@ import styled from "styled-components";
 import theme from "../../../styles/theme";
 
 export const GridContainer = styled.div`
-	height: 100vh;
-	width: 100%;
-	display: grid;
-	grid-template-columns: 30% 1fr 1fr;
-	grid-template-rows: 0.5fr 1.5fr 1fr;
-	grid-template-areas:
-		"Sidebar Header Header"
-		"Sidebar Section Section"
-		"Sidebar Section Section";
-	gap: 0px 0px;
+  height: 100vh;
+  width: 100%;
+  display: grid;
+  grid-template-columns: 30% 1fr 1fr;
+  grid-template-rows: 0.5fr 1.5fr 1fr;
+  grid-template-areas:
+    "Sidebar Header Header"
+    "Sidebar Section Section"
+    "Sidebar Section Section";
+  gap: 0px 0px;
 
-	.SidebarContainer {
-		grid-area: Sidebar;
-		box-shadow: ${theme.shadow};
-	}
+  .SidebarContainer {
+    grid-area: Sidebar;
+    box-shadow: ${theme.shadow};
+  }
 
-	.HeaderContainer {
-		grid-area: Header;
-	}
+  .HeaderContainer {
+    grid-area: Header;
+  }
 
-	.SectionContainer {
-		grid-area: Section;
-		padding-left: 7vw;
-		overflow-y: auto;
-	}
+  .SectionContainer {
+    grid-area: Section;
+    overflow-y: auto;
+  }
 
-	@media screen and (max-width: 768px) {
-		display: flex;
-		flex-direction: column;
+  @media screen and (max-width: 768px) {
+    display: flex;
+    flex-direction: column;
 
-		.SidebarContainer {
-			display: none;
-		}
+    .SidebarContainer {
+      display: none;
+    }
 
-		.SectionContainer {
-			grid-area: Section;
-			padding-left: 0vw;
-			overflow-y: auto;
-		}
-	}
+    .SectionContainer {
+      grid-area: Section;
+      padding-left: 0vw;
+      overflow-y: auto;
+    }
+  }
 `;
